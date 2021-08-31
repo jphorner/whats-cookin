777cd9efac8c3dcc2358acb53a0c16b22e642917
Renee Swick-Ziller <reneeswick@gmail.com>
2021-08-30 21:28:29 -0600

    Refactor test

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -2,116 +2,118 @@ import { expect } from 'chai';
 import RecipeRepository from '../src/classes/RecipeRepository';
 import Recipe from '../src/classes/Recipe';
 
-const recipeData = [
-  {
-  id: 595736,
-  image: 'https://spoonacular.com/recipeImages/595736-556x370.jpg',
-  ingredients: [
-    {
-      id: 20081,
-      quantity: {
-        amount: 1.5,
-        unit: 'c'
-      }
-    },
-    {
-      id: 18372,
-      quantity: {
-        amount: 0.5,
-        unit: 'tsp'
-      }
-    },
-  ],
-  instructions: [
-    {
-      instruction: 'In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.',
-      number: 1
-    },
-    {
-      instruction: 'Add egg and vanilla and mix until combined.',
-      number: 2
-    },
-  ],
-  name: 'Loaded Chocolate Chip Pudding Cookie Cups',
-  tags:[
-    'antipasti',
-    'starter',
-    'snack',
-    'appetizer',
-    'lunch'
-  ]
-},
-
-{
-  id: 412309,
-  image: 'https://spoonacular.com/recipeImages/412309-556x370.jpeg',
-  ingredients: [
-    {
-      id: 1002030,
-      quantity: {
-        amount: 4,
-        unit: 'teaspoons'
-      }
-    },
-    {
-      id: 19334,
-      quantity: {
-        amount: 8,
-        unit: 'tablespoons'
-      }
-    },
-  ],
-  instructions: [
-    {
-      instruction: 'Mix the hot sauce, butter, mango habanero sauce, brown sugar, chili powder, garlic powder, onion powder, black pepper, cayenne pepper and seasoning salt in a bowl. Stir vigorously until completely combined.',
-      number: 1
-    },
-  ],
-  name: 'Dirty Steve\s Original Wing Sauce',
-  tags:[
-    'sauce',
-  ]
-},
-{
-  id: 741603,
-  image: 'https://spoonacular.com/recipeImages/741603-556x370.jpeg',
-  ingredients: [
-    {
-      id: 20081,
-      quantity: {
-        amount: 1,
-        unit: 'cup'
-      }
-    },
-    {
-      id: 18371,
-      quantity: {
-        amount: 2,
-        unit: 'teaspoons'
-      }
-    },
-  ],
-  instructions: [
-    {
-      instruction: 'Watch how to make this recipe.',
-      number: 1
-    },
-    {instructions: 'In a large bowl, whisk together buttermilk, eggs, baking powder, sugar, salt and butter.',
-    number: 2
-    }
-  ],
-  name: 'Elvis Pancakes',
-  tags:[
-    'side dish',
-    'snack',
-  ]
-}
-];
 
 describe('Recipe', () => {
+  const recipeData = [
+    {
+    id: 595736,
+    image: 'https://spoonacular.com/recipeImages/595736-556x370.jpg',
+    ingredients: [
+      {
+        id: 20081,
+        quantity: {
+          amount: 1.5,
+          unit: 'c'
+        }
+      },
+      {
+        id: 18372,
+        quantity: {
+          amount: 0.5,
+          unit: 'tsp'
+        }
+      },
+    ],
+    instructions: [
+      {
+        instruction: 'In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.',
+        number: 1
+      },
+      {
+        instruction: 'Add egg and vanilla and mix until combined.',
+        number: 2
+      },
+    ],
+    name: 'Loaded Chocolate Chip Pudding Cookie Cups',
+    tags:[
+      'antipasti',
+      'starter',
+      'snack',
+      'appetizer',
+      'lunch'
+    ]
+  },
+
+  {
+    id: 412309,
+    image: 'https://spoonacular.com/recipeImages/412309-556x370.jpeg',
+    ingredients: [
+      {
+        id: 1002030,
+        quantity: {
+          amount: 4,
+          unit: 'teaspoons'
+        }
+      },
+      {
+        id: 19334,
+        quantity: {
+          amount: 8,
+          unit: 'tablespoons'
+        }
+      },
+    ],
+    instructions: [
+      {
+        instruction: 'Mix the hot sauce, butter, mango habanero sauce, brown sugar, chili powder, garlic powder, onion powder, black pepper, cayenne pepper and seasoning salt in a bowl. Stir vigorously until completely combined.',
+        number: 1
+      },
+    ],
+    name: 'Dirty Steve\s Original Wing Sauce',
+    tags:[
+      'sauce',
+    ]
+  },
+  {
+    id: 741603,
+    image: 'https://spoonacular.com/recipeImages/741603-556x370.jpeg',
+    ingredients: [
+      {
+        id: 20081,
+        quantity: {
+          amount: 1,
+          unit: 'cup'
+        }
+      },
+      {
+        id: 18371,
+        quantity: {
+          amount: 2,
+          unit: 'teaspoons'
+        }
+      },
+    ],
+    instructions: [
+      {
+        instruction: 'Watch how to make this recipe.',
+        number: 1
+      },
+      {instructions: 'In a large bowl, whisk together buttermilk, eggs, baking powder, sugar, salt and butter.',
+      number: 2
+      }
+    ],
+    name: 'Elvis Pancakes',
+    tags:[
+      'side dish',
+      'snack',
+    ]
+  }
+  ];
+  beforeEach(() => {
+    const recipeRepo = new RecipeRepository(recipeData);
+  });
 
   it('Should be a function', () => {
-    const recipeRepo = new RecipeRepository(recipeData);
     expect(RecipeRepository).to.be.a('function');
   });
 
